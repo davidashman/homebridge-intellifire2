1d6289aa816b00a98710a12c7ff5cdfc21d628c6
David Ashman <david.ashman@interfolio.com>
2023-12-20 00:18:33 -0500

    Logs for UDP

--- src/discovery.ts
+++ src/discovery.ts
@@ -20,16 +20,19 @@ export class Discovery {
 
     this.socket.bind(55555, () => {
       this.socket.setBroadcast(true);
+      this.platform.log.debug(`Sending UDP discovery packet`);
       this.socket.send('IFT-search', 3785, '255.255.255.255');
     });
   }
 
-  async handleDiscoveryPacket(msg, rinfo) {
+  async handleDiscoveryPacket(msg, _) {
+    this.platform.log.debug(`Received UDP packet for fireplace: ${msg}`);
     const data = JSON.parse(msg) as DiscoveryInfo;
     fetch(`http://${data.ip}/poll`)
       .then((response) => {
         if (response.ok) {
           response.json().then((json) => {
+            this.platform.log.debug(`Tracking fireplace ${json.serial} at ip ${data.ip}`);
             this.ipList.set(json.serial, data.ip);
           })
         }
